70b4b488a6461fca8ac78898fdb2e16fb5ece303
John Knotts <jhnnyk@gmail.com>
2020-12-22 18:14:42 -0700

    following some lessons, just watching others

---
+++ components/TheNavbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav class="nav">
+    <div class="logo">
+      <nuxt-link :to="{ name: 'index' }" class="logo text-lg">
+        Nuxt Fundamentals
+      </nuxt-link>
+
+      <span class="subheader">a Vue School course</span>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {}
+</script>
